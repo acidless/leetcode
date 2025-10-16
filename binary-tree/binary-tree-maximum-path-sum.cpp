62c149907e9b9e976fdb472a918d9b5b68caff91
Oleg Kukunov <odkukunov@gmail.com>
2025-10-16 12:00:12 +0300

    Create binary-tree-maximum-path-sum.cpp

---
+++ binary-tree/binary-tree-maximum-path-sum.cpp
@@ -0,0 +1,35 @@
+// Problem: Binary Tree Maximum Path Sum
+// Link: https://leetcode.com/problems/binary-tree-maximum-path-sum
+// Approach: Recursive, O(n)
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxPathSum(TreeNode* root) {
+        int maxSum = numeric_limits<int>::min();
+        dfs(root, maxSum);
+        return maxSum;
+    }
+private:
+    int dfs(TreeNode* root, int& maxSum) {
+        if(!root) {
+            return 0;
+        }
+
+        int leftMax = max(dfs(root->left, maxSum), 0);
+        int rightMax = max(dfs(root->right, maxSum), 0);
+
+        maxSum = max(maxSum, root->val + leftMax + rightMax);
+        return root->val + max(leftMax, rightMax);
+    }
+};
