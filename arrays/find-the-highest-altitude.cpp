dc5a8e8e9f50b5fa63b9ee1d79afbaff18aaab2c
Oleg Kukunov <odkukunov@gmail.com>
2025-10-17 23:58:26 +0300

    Create find-the-highest-altitude.cpp

---
+++ arrays/find-the-highest-altitude.cpp
@@ -0,0 +1,19 @@
+// Problem: Find the Highest Altitude
+// Link: https://leetcode.com/problems/find-the-highest-altitude
+// Approach: O(n)
+
+
+class Solution {
+public:
+    int largestAltitude(vector<int>& gain) {
+        int maxAlt = 0;
+        int currentAlt = 0;
+        
+        for(int i = 0; i < gain.size(); i++) {
+            currentAlt += gain[i];
+            maxAlt = max(maxAlt, currentAlt);
+        }
+
+        return maxAlt;
+    }
+};
