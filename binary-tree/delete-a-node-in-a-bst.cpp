0bd7e964d988ac4091c70a2fbbfeb72715138558
Oleg Kukunov <odkukunov@gmail.com>
2025-10-26 13:11:37 +0300

    Create delete-a-node-in-a-bst.cpp

---
+++ binary-tree/delete-a-node-in-a-bst.cpp
@@ -0,0 +1,58 @@
+// Problem: Delete Node in a BST
+// Link: https://leetcode.com/problems/delete-node-in-a-bst
+// Approach: O(h)
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        if (root == nullptr) {
+            return root;
+        }
+
+        if (root->val > key) {
+            root->left = deleteNode(root->left, key);
+        }
+        else if (root->val < key) {
+            root->right = deleteNode(root->right, key);
+        }
+        else {
+            if (root->left == nullptr) {
+                TreeNode* temp = root->right;
+                delete root;
+                return temp;
+            }
+
+            if (root->right == nullptr) {
+                TreeNode* temp = root->left;
+                delete root;
+                return temp;
+            }
+
+            TreeNode* succ = getSuccessor(root);
+            root->val = succ->val;
+            root->right = deleteNode(root->right, succ->val);
+        }
+
+        return root;
+    }
+private:
+    TreeNode* getSuccessor(TreeNode* curr) {
+        curr = curr->right;
+        while (curr != nullptr && curr->left != nullptr) {
+            curr = curr->left;
+        }
+
+        return curr;
+    }
+};
